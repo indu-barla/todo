af1d8f41f595ca41934e9b992b4eaac23bc156aa
Indu Barla <indu@Indus-MacBook-Air.local>
2022-10-26 20:00:21 +0530

    todo

---
+++ src/components/todoList.js
@@ -0,0 +1,21 @@
+import React from "react";
+//Import Components
+import Todo from "./Todo";
+
+
+const TodoList = ({todos}) => {
+
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+               {todos.map(todos => (
+                <Todo text={todos.text} />
+               ))} 
+            </ul>
+        </div>    
+   
+    );
+};
+
+export default TodoList;
+
